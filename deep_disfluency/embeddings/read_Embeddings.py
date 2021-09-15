0036f73b609e0ec1ead794e941ba164e39ddf5de
treena <treena908@gmail.com>
2021-09-15 12:35:26 -0500

    reading embeddings fine

--- deep_disfluency/embeddings/read_Embeddings.py
+++ deep_disfluency/embeddings/read_Embeddings.py
@@ -1,4 +1,6 @@
 import gensim
 from gensim.models import KeyedVectors
+
 pretrained = KeyedVectors.load('db_clean_50')
 print(pretrained)
+print ("emb shape"+ str(pretrained[pretrained.wv.index2word[0]].shape))
